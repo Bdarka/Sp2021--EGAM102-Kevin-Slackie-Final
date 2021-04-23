03a72e4efbfab0729fb338506d26626cc8cc955e
Bdarka <70989149+Bdarka@users.noreply.github.com>
2021-04-23 14:31:33 -0700

    Commitment Issues

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 1f;
+
     public int Health;
     public GameObject Bullet;
     Rigidbody2D rb;
+    BoxCollider2D bc;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.MovePosition(transform.position + .025f * Vector3.right);
+       if(IsFacingLeft())
+        {
+            // Move Right
+            rb.velocity = new Vector2(-moveSpeed, 0f);
+        }
+        else
+        {
+            // Move Left 
+            rb.velocity = new Vector2(moveSpeed, 0f);
+        }
     }
 
     public void Damage()
@@ -27,5 +40,16 @@ public class Enemy : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //Turn 
+        transform.localScale = new Vector2((Mathf.Sign(rb.velocity.x)), transform.localScale.y);
+    }
+
+    private bool IsFacingLeft()
+    {
+        return transform.localScale.x > -Mathf.Epsilon; // Checks for an incredibly small value, better than putting .0001
     }
 }
