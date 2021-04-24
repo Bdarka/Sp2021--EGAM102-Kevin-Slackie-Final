97bf9cbcee7c99a8d9320b8c5c55be10be75e23e
Bdarka <70989149+Bdarka@users.noreply.github.com>
2021-04-23 17:42:25 -0700

    Hopefully Final Changes

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -10,7 +10,7 @@ public class BulletScript : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(1, 0);
+        rb.velocity = new Vector2(2.5f, 0);
     }
 
     // Update is called once per frame
@@ -26,16 +26,19 @@ public class BulletScript : MonoBehaviour
             {
                 col.gameObject.GetComponent<Enemy>().Damage();
 
-                Destroy(gameObject);
+                Destroy(this.gameObject);
             }
-        
+        if(col.gameObject.tag == "floor")
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D col)
     {
         if (col.gameObject.tag == "Boundary")
         {
-            Destroy(gameObject);
+            Destroy(this.gameObject);
         }
     }
 
